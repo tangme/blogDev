d15cabc327906734939edbc3ad72a8ec44adee37
tanglv <tangchaofalv@163.com>
2021-06-29 17:00:11 +0800

    测试

--- scripts/t.js
+++ scripts/t.js
@@ -1,1 +1,2 @@
 hexo.extend.injector.register("head_begin", '<link rel="manifest" href="/blog/manifest.json">', "default");
+hexo.extend.injector.register("head_begin", '<script  src="/blog/service-worker.js" ></script>', "default");
